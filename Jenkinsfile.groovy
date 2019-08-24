c78e15e5f30e873205010a5b3eafd9fe3b46825d
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-24 11:24:15 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,8 @@
 node {
     stage("Say Hello"){
+    properties([parameters([text(defaultValue: '54.202.189.136', description: '''Dev : 54.202.189.136 Qa : 54.202.234.2 Prod : 54.70.46.105''', name: 'Remote_Instance')])])
     git 'https://github.com/Khuslentuguldur/jenkins_august.git'
-      
+
 
     }
 }
