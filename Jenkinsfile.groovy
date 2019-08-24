dc767d881f6b88e698831aaf6d280d80c3887488
khuslentuguldur <khuslen.cyber@gmail.com>
2019-08-24 11:08:37 -0500

    updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node {
     stage("Say Hello"){
-      sh "echo Hello ${NAME}"
+      sh "echo Hello ${NAME}",
       properties([parameters([text(defaultValue: 'Khuslen', description: 'Please put your name.', name: 'NAME ')]), pipelineTriggers([cron('* * * * *')])])
     }
 }
